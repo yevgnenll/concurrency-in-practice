007958d98d1787b7f71ff78a024aaecab4b387f0
cw-seungkwon <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>
2021-07-24 03:50:08 +0900

    라이브러리 살펴보기

---
+++ src/main/java/me/yevgnenll/concurrent/serializable/JavaPackageList.java
@@ -0,0 +1,22 @@
+package me.yevgnenll.concurrent.serializable;
+
+import java.awt.*;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class JavaPackageList {
+
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+
+        // value class
+        BigInteger bigInteger;
+        ReentrantLock reentrantLock;
+
+        // 상속용
+        Throwable throwable;
+        Component component;
+    }
+}
